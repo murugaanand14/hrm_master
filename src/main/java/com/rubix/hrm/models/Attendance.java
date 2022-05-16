9fdd62bb6323cd24de758c056a6da71838a295f5
Karthick Kumar <karthick.s@rubixtek.com>
2022-05-16 17:00:49 +0530

    Modified Attendance model file

--- src/main/java/com/rubix/hrm/models/Attendance.java
+++ src/main/java/com/rubix/hrm/models/Attendance.java
@@ -6,38 +6,25 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.Data;
-=======
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
->>>>>>> e3a6c833762be05ce0ae6b472d530086c0be6535
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "attendance")
 public class Attendance {
-<<<<<<< HEAD
-	
-	
-=======
 
 
->>>>>>> e3a6c833762be05ce0ae6b472d530086c0be6535
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "reference_id")
@@ -50,8 +37,5 @@ public class Attendance {
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
     @Column(name = "outtime")
     private Date outTime;
-<<<<<<< HEAD
-=======
 
->>>>>>> e3a6c833762be05ce0ae6b472d530086c0be6535
 }
